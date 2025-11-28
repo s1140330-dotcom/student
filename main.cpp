7108cf07d5131c23e7924002394cfd9f016f8309
s1140330-dotcom <s1140330@dmhs.kh.edu.tw>
2025-11-28 16:15:40 +0800

    hw8
    hw8

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    int a =8;
-    int b =3;
-    int c =a-b;
-    
-    std::cout <<(a>b) << "/t";
-    std::cout <<(a<c) << "/t";
-    std::cout <<(a!=b) << "/t";
-    std::cout <<(a== b) << "/t";
-    std::cout <<(a>=b) << "/t";
+int main() {
+    int sum = 0;
 
+    for (int i = 1; i <= 5; i++) {
+        sum += i;   // sum = sum + i
+    }
 
+    cout << "1 + 2 + 3 + 4 + 5 = " << sum << endl;
     return 0;
 }
