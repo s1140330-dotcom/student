eac0be1ba68448d068e688fea3bd2607a6a7f571
s1140330-dotcom <s1140330@dmhs.kh.edu.tw>
2025-12-04 09:38:18 +0800

    hw9
    hw9

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-
-    for (int i = 1; i <= 5; i++) {
-        sum += i;   // sum = sum + i
+    // 外層迴圈控制行數 (1到9)
+    for (int i = 1; i <= 9; ++i) {
+        // 內層迴圈控制列數 (1到9)
+        for (int j = 1; j <= 9; ++j) {
+            // 輸出乘法表的每一項，使用 "\t" 來對齊
+            cout << i << " * " << j << " = " << i * j << "\t";
+        }
+        // 每列後換行
+        cout << endl;
     }
-
-    cout << "1 + 2 + 3 + 4 + 5 = " << sum << endl;
     return 0;
 }
